21758cda3b47735f65bb092c03bbedde3dcd575f
Anass Cheab <anassalien123@gmail.com>
2024-12-05 02:40:58 +0100

    new work

---
+++ js/main.js
@@ -0,0 +1,10 @@
+// open & close Cart
+
+var cart = document.querySelector('.cart');
+
+function open_cart(){
+    cart.classList.add("active");
+}
+function close_cart(){
+    cart.classList.remove("active");
+}
