1ba7b51515ff4d1548f41a538c63fd83c4980407
Anass Cheab <anassalien123@gmail.com>
2025-01-05 00:39:30 +0100

    New

---
+++ js/swiper.js
@@ -0,0 +1,31 @@
+/* swiper slider  side-bar */
+
+/* Initialize Swiper */
+
+var swiper = new Swiper(".slide-swp", {
+    pagination: {
+    el: ".swiper-pagination",
+    DynamicBullests: true,
+    clickable: true
+},
+autoplay:{
+    delay:2500,
+},
+loop:true,
+});
+
+/* swiper slider  sale_slide */
+
+/* Initialize Swiper */
+
+var swiper = new Swiper(".sale_sec", {
+    pagination: {
+    el: ".swiper-pagination",
+    DynamicBullests: true,
+    clickable: true
+},
+autoplay:{
+    delay:2500,
+},
+loop:true,
+});
